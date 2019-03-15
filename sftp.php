43de5ad34f8b22a04538cdfe4c4be2921820711f
peter279k <peter279k@gmail.com>
2019-03-15 17:29:32 +0800

    Test sftp via phpseclib

--- sftp.php
+++ sftp.php
@@ -2,10 +2,17 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use phpseclib\Crypt\RSA;
 use phpseclib\Net\SFTP;
 
 $sftp = new SFTP('localhost');
 
-if (!$sftp->login('travis', 'travis')) {
+$Key = new RSA();
+// If the private key has a passphrase we set that first
+$Key->setPassword('');
+// Next load the private key using file_gets_contents to retrieve the key
+$Key->loadKey(file_get_contents('~/.ssh/id_rsa'));
+
+if (!$sftp->login('travis', $Key)) {
 	throw new Exception('Login failed');
 }
