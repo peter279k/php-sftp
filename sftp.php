a72f9155cbbe32f16062a48bab36efef7d7a573a
peter279k <peter279k@gmail.com>
2019-03-15 17:33:35 +0800

    Change the current private key path

--- sftp.php
+++ sftp.php
@@ -11,7 +11,7 @@ $Key = new RSA();
 // If the private key has a passphrase we set that first
 $Key->setPassword('');
 // Next load the private key using file_gets_contents to retrieve the key
-$Key->loadKey(file_get_contents('~/.ssh/id_rsa'));
+$Key->loadKey(file_get_contents('/home/travis/.ssh/id_rsa'));
 
 if (!$sftp->login('travis', $Key)) {
 	throw new Exception('Login failed');
